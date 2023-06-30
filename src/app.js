7a48780b61a9ec3e014c1488be58d09adf9f94ac
lucphyl <107371429+lucphyl@users.noreply.github.com>
2023-06-30 03:29:44 -0300

    basic schemas

--- src/app.js
+++ src/app.js
@@ -1,28 +1,40 @@
-import cors from 'cors'
-import dotenv from 'dotenv'
-import express from 'express'
-import { MongoClient } from 'mongodb'
+import cors from "cors";
+import dayjs from "dayjs";
+import dotenv from "dotenv";
+import express from "express";
+import { MongoClient, ObjectId } from "mongodb";
+import joi from "joi";
 
-const App = express
+const App = express();
 
 //config
 App.use(cors());
 App.use(express.json());
 dotenv.config();
 
-//schemas
-App.length("/teste", (request,response) => {
-    response.send("tá lá");
+// Schemas
+const ParticipantsPreset = joi.object({name: joi.string().required()} );
+const messagePreset = joi.object({
+    from: joi.string().required(),
+    type: joi.string().required(),
+    to: joi.string().required(),
+    text: joi.string().required()
 });
 
-//mongo connection
+
+// mongo connection 
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 try {
-    await mongoClient.connect();
-    console.log("conectado");
- } catch (error) {
-    console.log(error.message);
- };
+    await mongoClient.connect()
+    console.log('Mongo tá on')
+} catch (error) {
+    console.log(error.message)
+}
+
 const db = mongoClient.db();
 
 
+
+// PORT
+const PORT = 5000;
+App.listen(PORT, () => console.log(`Servidor aberto na porta: ${PORT}`));
